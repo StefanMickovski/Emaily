df16cb43a7b8a86eeec843d40c98e2b830940c3f
Stefan Mickovski <stefan.mickovski@hotmail.com>
2024-11-27 18:43:13 +0100

    Implementing the React Setup

---
+++ server/client/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+
+import App from './components/App';
+
+const store = createStore(() => [], {}, applyMiddleware());
+ 
+
+const el = document.getElementById("root");
+ 
+
+const root = ReactDOM.createRoot(el);
+ 
+
+root.render(<App />, document.querySelector("#root"));
