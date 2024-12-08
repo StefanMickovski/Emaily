27d995dd40e32ee14755abe5ee112189d26c5658
Stefan Mickovski <stefan.mickovski@hotmail.com>
2024-12-08 15:07:07 +0100

    Added couple of new funcitonalities on the back

--- server/index.js
+++ server/index.js
@@ -2,16 +2,21 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const bodyParser = require("body-parser");
+const cors = require("cors"); 
 const keys = require('./config/keys');
 require("./models/User");
+require("./models/Survey");
 require("./services/passport");
-
-
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
 
+
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));  
+
+app.use(bodyParser.json());
 app.use(
     cookieSession({
         maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -22,6 +27,19 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
