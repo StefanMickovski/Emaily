320103e16b19aa27bce05b32f3d854f726a75bab
Stefan Mickovski <stefan.mickovski@hotmail.com>
2024-12-09 14:23:22 +0100

    Inserted the surveys component

--- server/client/src/components/Dashboard.js
+++ server/client/src/components/Dashboard.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
     return (
         <div style={{ textAlign: "center" }}>
-            <h1>
-                Emaily Dashboard!
-            </h1>
-            Collect feedback from your users
+            Dashboard
+            <div className="fixed-action-btn">
+                <Link to="/surveys/new" className="btn-floating btn-large red">
+                    <i className="material-icons">add</i>
+                </Link>
+            </div>
         </div>
     );
 };
